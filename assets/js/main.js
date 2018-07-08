d52bd430239380bf1dbb91c0fb5f6176fae2008f
F1r3byte <f1r3byte@yandex.com>
2018-07-08 14:15:15 +0300

    added community links

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	const bottomText = document.getElementsByClassName('bottomText')[0];
 	const ztmLogo = document.getElementsByClassName('ztmLogo')[0];
 
-
 	bottomText.addEventListener('click', goToZTM);
 	ztmLogo.addEventListener('click', goToZTM);
 });
