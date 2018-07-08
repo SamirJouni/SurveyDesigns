5e209d79663f36442cdbe3f7b0ec0f89b930a542
F1r3byte <f1r3byte@yandex.com>
2018-07-08 13:51:29 +0300

    linked to the ztm community

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
+	const madeWithLove = document.getElementsByClassName('bottomText')[0];
 
-}
+	madeWithLove.addEventListener('click', function () {
+		window.location.href= 'https://github.com/zero-to-mastery';
+	});
+});
