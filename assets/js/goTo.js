09f4715fa3a0e20450389e03ba1daf7076095cc1
F1r3byte <f1r3byte@yandex.com>
2018-07-09 21:06:30 +0200

    made some logical changes to js and linked create survey js

--- assets/js/goTo.js
+++ assets/js/goTo.js
@@ -2,3 +2,7 @@ function goToZTM () {
 	"use strict";
 	window.location.href = 'https://github.com/zero-to-mastery';
 }
+function newSurvey () {
+	"use strict";
+	window.location.href = 'assets/html/createSurvey.html';
+}
