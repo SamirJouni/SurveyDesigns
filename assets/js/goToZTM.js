c260b077433d6da4d10cac5f80079a7bb080c4ed
F1r3byte <f1r3byte@yandex.com>
2018-07-11 21:20:15 +0000

    fixed js bug and refactored some code

--- assets/js/goToZTM.js
+++ assets/js/goToZTM.js
@@ -2,7 +2,3 @@ function goToZTM () {
 	"use strict";
 	window.location.href = 'https://github.com/zero-to-mastery';
 }
-function newSurvey () {
-	"use strict";
-	window.location.href = 'assets/html/createSurvey.html';
-}
