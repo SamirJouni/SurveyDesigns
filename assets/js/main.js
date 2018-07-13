39a27d66b87dc0aa32443f0a723065a467e2d1fd
F1r3byte <f1r3byte@yandex.com>
2018-07-13 23:58:23 +0200

    fixed a bug and refactored some code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	const bottomText = document.getElementsByClassName('bottomText')[0];
 	const ztmLogo = document.getElementsByClassName('ztmLogo')[0];
-	const createSurvey = document.getElementsByClassName('createSurvey')[0];
 
 	bottomText.addEventListener('click', goToZTM);
 	ztmLogo.addEventListener('click', goToZTM);
-	createSurvey.addEventListener('click', newSurvey);
 });
