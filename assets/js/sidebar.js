3ff6171930114193cac3ad9e9f4abe85e00547a2
F1r3byte <f1r3byte@yandex.com>
2018-07-20 23:31:12 +0200

    updated sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
+	const allSurveys = document.getElementById('allSurveys');
+	const mySurveys = document.getElementById('mySurveys');
+	const myAccount = document.getElementById('myAccount');
+	const projectRepo = document.getElementById('projectRepo');
+
+	const createSurvey = document.getElementsByClassName('createSurvey')[0];
+
+
+
+allSurveys.addEventListener('click', function(){
+	if (!createSurvey){
+		window.location.href = "allSurveys.html"
+}
+	else {
+		window.location.href = "assets/html/allSurveys.html"
+	}
+});
+
 });
