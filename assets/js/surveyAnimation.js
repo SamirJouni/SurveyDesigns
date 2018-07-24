63379f17d2666fd098e27f6133de9f24351b51db
F1r3byte <f1r3byte@yandex.com>
2018-07-24 23:41:40 +0200

    created the animation for the surveys

---
+++ assets/js/surveyAnimation.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+	"use strict";
+	const surveys = Array.from(document.getElementsByClassName('survey'));
+
+
+  surveys.forEach(function(survey) {
+
+		survey.addEventListener('click', function() {
+
+        survey.classList.add('zoomInOut', 'transparent');
+
+				setTimeout(function() {
+          survey.classList.remove('zoomInOut', 'transparent');
+				}, 200);
+			});
+		});
+
+			});
