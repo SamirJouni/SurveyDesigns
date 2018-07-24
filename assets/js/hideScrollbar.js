f75ba9332a24bdf758318e9d6aab0761d574292b
F1r3byte <f1r3byte@yandex.com>
2018-07-24 23:24:13 +0200

    improved security

--- assets/js/hideScrollbar.js
+++ assets/js/hideScrollbar.js
@@ -1,4 +1,9 @@
-var parent = document.getElementsByClassName('contentAndTitles')[0];
-var child = document.getElementsByClassName('surveyList')[0];
-child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
-child.style.paddingBottom = child.offsetHeight - child.clientHeight + "px";
+document.addEventListener('DOMContentLoaded', function () {
+	"use strict";
+	const parent = document.getElementsByClassName('contentAndTitles')[0];
+	const child = document.getElementsByClassName('surveyList')[0];
+
+	child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
+	child.style.paddingBottom = child.offsetHeight - child.clientHeight + "px";
+
+});
