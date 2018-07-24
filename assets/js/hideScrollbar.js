9e751bcd477e53392a9097d995a9324400c10c33
F1r3byte <f1r3byte@yandex.com>
2018-07-24 23:22:34 +0200

    hid the scrollbar

---
+++ assets/js/hideScrollbar.js
@@ -0,0 +1,4 @@
+var parent = document.getElementsByClassName('contentAndTitles')[0];
+var child = document.getElementsByClassName('surveyList')[0];
+child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
+child.style.paddingBottom = child.offsetHeight - child.clientHeight + "px";
