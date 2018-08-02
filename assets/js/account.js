d6522f8957716686b691a413f38306d05c932929
F1r3byte <f1r3byte@yandex.com>
2018-08-02 19:20:45 +0200

    added some js and made improvements

--- assets/js/account.js
+++ assets/js/account.js
@@ -5,7 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	const username = document.getElementById('username');
 	const changeUsername = document.getElementById('changeUsername');
+	const usernameInput = document.getElementById('usernameInput');
 	const email = document.getElementById('email');
 	const changeEmail = document.getElementById('changeEmail');
+	const emailInput = document.getElementById('emailInput');
+
+
+	 changeUsername.addEventListener('click', function (){
+
+		username.classList.toggle('displayNone');
+		usernameInput.classList.toggle('displayNone');
+
+		if ( usernameInput.value) {
+
+		}
+
+	 });
+
+	 changeEmail.addEventListener('click', function (){
+
+		email.classList.toggle('displayNone');
+		emailInput.classList.toggle('displayNone');
+
+	 });
+
 
 });
