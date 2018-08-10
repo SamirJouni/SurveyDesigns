dd024a5c0027c5af9f6c3cb1bf09f19ae9b888bd
F1r3byte <f1r3byte@yandex.com>
2018-08-10 20:16:26 +0200

    improved the description of a comment

--- assets/js/createSurvey.js
+++ assets/js/createSurvey.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const modalTextContent = document.getElementById('modalTextContent');
 
 
-/* Add an event listener to open modal when a button is clicked */
+/* Add an event listener to open modal when a button among the question buttons is clicked */
 	for (var i = 0; i < NumberOfButtons; i++) {
 
 		buttons[i].addEventListener('click', function (button) {
