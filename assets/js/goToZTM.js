364b3ddfff46423869a58deffbe4cff31b1ebe85
F1r3byte <f1r3byte@yandex.com>
2018-08-11 20:13:19 +0200

    added comment to js

--- assets/js/goToZTM.js
+++ assets/js/goToZTM.js
@@ -1,5 +1,7 @@
 /* This is function that redirects to the ztm community github repo */
 function goToZTM () {
 	"use strict";
+
+	/* Load the zero to mastery github organization page */
 	window.location.href = 'https://github.com/zero-to-mastery';
 }
