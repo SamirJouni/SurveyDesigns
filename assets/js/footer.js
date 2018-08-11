9dc8548cc0f81a3a8569ca1189af170861d1d4d8
F1r3byte <f1r3byte@yandex.com>
2018-08-11 20:11:32 +0200

    added comment to js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const bottomText = document.getElementsByClassName('bottomText')[0];
 	const ztmLogo = document.getElementsByClassName('ztmLogo')[0];
 
+	/* go to zero to mastery repo on github, when the community logo, or the text below it are clicked */
 	bottomText.addEventListener('click', goToZTM);
 	ztmLogo.addEventListener('click', goToZTM);
 });
