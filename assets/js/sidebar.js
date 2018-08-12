7d52d1b07c9f15e18ab68307bd33cb568fbb0a7a
F1r3byte <f1r3byte@yandex.com>
2018-08-12 20:11:44 +0200

    added comment to js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -21,6 +21,8 @@ allSurveys.addEventListener('click', function(){
 	}
 });
 
+/* event listener that redirects to a page that displays all surveys that the user has created,
+when the my surveys button in the sidebar is pressed */
 mySurveys.addEventListener('click', function(){
 	if (!createSurvey){
 		window.location.href = "mySurveys.html"
