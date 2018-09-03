e0ce30c24e4d92e884bac332652317e95ccadd6a
F1r3byte <f1r3byte@yandex.com>
2018-09-03 20:39:26 +0200

    added a way for question one to be created

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -8,6 +8,22 @@ function createPreview ( questionType, questionObject ) {
 	const pRoot = document.getElementById('pRoot');
 
 	if (questionType === 1 ) {
+		const content1 = document.createElement('div');
+		const content2 = document.createElement('div');
+		const content3 = document.createElement('span');
+		const content4 = document.createElement('div');
+		const content5 = document.createElement('input');
+
+		content1.classList.add('previewQuestion')
+		content3.textContent = questionObject.question;
+		content5.placeholder = 'And Your Answer is...';
+
+		content2.appendChild(content3);
+		content4.appendChild(content5);
+		content1.appendChild(content2);
+		content1.appendChild(content4);
+
+		pRoot.appendChild(content1);
 
 	} else if (questionType === 2 ) {
 
