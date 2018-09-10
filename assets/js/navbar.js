2b3aa28ada98d68cbf2700e9fac5bc983914862a
F1r3byte <f1r3byte@yandex.com>
2018-09-10 14:39:11 +0200

    added working designs

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,17 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
 
 	const logo = document.getElementsByClassName('logoWithText')[0];
-	const projectRepo = document.getElementById('projectRepo');
 
 	/* Take user to homepage when the logo in the left corner of the navbar is clicked */
-	logo.addEventListener('click', function () {
+	logo.addEventListener('click', function(){
 
-		window.location.href = "../../index.html"
-	});
-
-	/* event listener that opens the page of the repo of this project when the github logo
-	is pressed */
-	projectRepo.addEventListener('click', function () {
-		window.location.href = "https://github.com/zero-to-mastery/TheVerySpecialProject"
+		window.location.href= "../../index.html"
 	});
 });
