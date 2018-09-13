a537354b15991066b1fbdfc1c60a80c4bd27d520
065rsh <gogl.arshdeep@gmail.com>
2018-09-13 17:28:58 +0530

    chaged some javascipt

--- assets/js/createSurvey.js
+++ assets/js/createSurvey.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const surveyTitlePreview = document.getElementById('surveyTitlePreview');
 	let questionId = 0 ;
 
-	let firstClick = true;
+	let firstClick = [true,true,true];
 	/* Add an event listener to open modal when a button among the question creation buttons is clicked */
 	for (var i = 0; i < NumberOfButtons; i++) {
 		buttons[i].addEventListener('click', function (button) {
@@ -37,19 +37,21 @@ document.addEventListener('DOMContentLoaded', function () {
 	function createSpecificModal(buttonId) {
 
 		/* specific modal creation if the "add a question" button was pressed */
-		if(buttonId[1] === '0' && firstClick){
+		if(buttonId[1] === '0' && firstClick[0]){
+
+			closeAll(1, buttonId[1]);
+			closeAll(2, buttonId[1]);
+			
 			const currentdropdown = dropdown[buttonId[1]];
 			
 			$( currentdropdown ).animate({ height: "8vh" }, 200 );
-			// currentdropdown.style.height = "8vh";
-			console.log(currentdropdown.children);
 			if(currentdropdown.children.length === 0) {
 				const content1 = document.createElement('label');
 				const content2 = document.createElement('input');
 				const content3 = document.createElement('div');
 
-				content1.textContent = "Q: ";
-				content2.placeholder = "Enter your question here ";
+				content1.textContent = "Q" + (questionId + 1) + ": ";
+				content2.placeholder = "Enter your question here";
 				content3.textContent = "Done";
 				currentdropdown.appendChild(content1);
 				currentdropdown.appendChild(content2);
@@ -71,20 +73,21 @@ document.addEventListener('DOMContentLoaded', function () {
 				$(currentdropdown.children[1]).focus();
 				currentdropdown.children[1].minLength = "10";
 				currentdropdown.children[1].title = "minimum character length of a question should be " + currentdropdown.children[1].minLength;
-				currentdropdown.children[1].size = "30";
+				currentdropdown.children[1].size = "39";
 				currentdropdown.children[1].addEventListener("input", onChange);
 				// run when enter is pressed inside the input field
 				currentdropdown.children[1].addEventListener("keyup", function(e) {
 					if(e.keyCode == '13' && $(currentdropdown.children[1]).is(':valid') && this.value.length) doneClicked();
 				});
 				function onChange(value) {
-					currentdropdown.children[1].size = "30";
+					currentdropdown.children[1].size = "39";
 					if($(currentdropdown.children[1]).is(':valid') && this.value.length) {
 						input.borderBottom = ".3vh solid #77bb00";
 						viewDone(content3);
-						if(this.value.length > 25){
-							currentdropdown.children[1].size = this.value.length + 12;
-						}
+						if (this.value.length > 9 && this.value.length < 50)
+							currentdropdown.children[1].size = this.value.length + 30;
+						else 
+							currentdropdown.children[1].size = 80;
 					} else {
 						input.borderBottom = ".3vh solid #888";
 						if(buttons[buttonId[1]].lastChild.textContent === content3.textContent) {
@@ -104,13 +107,103 @@ document.addEventListener('DOMContentLoaded', function () {
 						questionId++;
 						modal.style.display = "none";
 					}
-					closeAll(buttonId[1]);
+					closeAll(buttonId[1], buttonId[1]);
 				}
 			}
-			firstClick = false;
+			firstClick[0] = false;
+			firstClick[1] = true;
+		}
+
+		// clicked single choice question button 
+		else if (buttonId[1] === '1' && firstClick[1]) {
+			var optionCount = 1;
+			closeAll(0, buttonId[1]);
+			closeAll(2, buttonId[1]);
+
+			const currentdropdown = dropdown[buttonId[1]];
+			
+			$( currentdropdown ).animate({ height: "8vh" }, 200 );
+			if(currentdropdown.children.length === 0) {
+				const content1 = document.createElement('label');
+				const content2 = document.createElement('input');
+				const content3 = document.createElement('div');
+				const content4 = document.createElement('label');
+				const content5 = document.createElement('input');
+
+				content1.textContent = "Q" + (questionId + 1) + ": ";
+				content2.placeholder = "Enter your question here";
+				content3.textContent = "Done";
+				content4.textContent = optionCount + ")";
+				content5.placeholder = "Enter your option";
+
+				currentdropdown.appendChild(content1);
+				currentdropdown.appendChild(content2);
+				currentdropdown.appendChild(content4);
+				currentdropdown.appendChild(content5);
+
+				currentdropdown.style.paddingLeft = "7vw";
+
+				currentdropdown.children[0].style.fontStyle = "italic";
+
+				const input = currentdropdown.children[1].style;
+				input.fontFamily = "'Montserrat',sans-serif";
+				input.marginLeft = "1vw";
+				input.height = "4vh";
+				input.paddingLeft = ".5vw";
+				input.color = "#555";
+				input.border = "0";
+				input.borderBottom = ".3vh solid #888";
+
+				// children[1] is for input box
+				$(currentdropdown.children[1]).focus();
+				currentdropdown.children[1].minLength = "10";
+				currentdropdown.children[1].title = "minimum character length of a question should be " + currentdropdown.children[1].minLength;
+				currentdropdown.children[1].size = "39";
+				currentdropdown.children[1].addEventListener("input", onChange);
+				// run when enter is pressed inside the input field
+				currentdropdown.children[1].addEventListener("keyup", function(e) {
+					if(e.keyCode == '13' && $(currentdropdown.children[1]).is(':valid') && this.value.length) doneClicked();
+				});
+				function onChange(value) {
+					currentdropdown.children[1].size = "39";
+					if($(currentdropdown.children[1]).is(':valid') && this.value.length) {
+						input.borderBottom = ".3vh solid #77bb00";
+						viewDone(content3);
+						if (this.value.length > 9 && this.value.length < 50)
+							currentdropdown.children[1].size = this.value.length + 30;
+						else 
+							currentdropdown.children[1].size = 80;
+					} else {
+						input.borderBottom = ".3vh solid #888";
+						if(buttons[buttonId[1]].lastChild.textContent === content3.textContent) {
+							buttons[buttonId[1]].removeChild(buttons[buttonId[1]].lastChild);
+						}
+					}
+				}
+				content3.addEventListener('click', function() {
+					doneClicked();
+				});
+				function doneClicked(){
+					if (content2.value) {
+						const questionObject = {
+							question: content2.value.includes('?') ? content2.value : content2.value + '?',
+						}
+						createPreview(1, questionObject, questionId);
+						questionId++;
+						modal.style.display = "none";
+					}
+					closeAll(buttonId[1], buttonId[1]);
+				}
+			}
+			firstClick[1] = false;
+			firstClick[0] = true;
 		} else {
-			closeAll(buttonId[1]);
-		} function viewDone(c){
+			for (i = 0; i < firstClick.length; i++){
+				closeAll(i, i);
+			}
+		}
+
+		function viewDone(c){
 			buttons[buttonId[1]].appendChild(c);
 			c.style.boxShadow = "0 0 0.5vw 0 #aaa";
 			c.style.padding = "0.5vw 0.8vw";
@@ -121,59 +214,23 @@ document.addEventListener('DOMContentLoaded', function () {
 			c.style.color = "#77bb00";	
 		}
 	}
-	function closeAll(b, bool) {
-		for(var i = 0; i < dropdown.length; i++) {
-			$( dropdown[i] ).animate({ height: "0" }, 200 );
-			const d = dropdown[i];
-			setTimeout(ontimeout, 250);
-			function ontimeout() {
-				while(d.firstChild) {
-					d.removeChild(d.firstChild);
-				}
+
+	 // a is the dropdown which needs to be closed and b is the button which is clicked
+	 // firstClick false means that the dropdown is open
+	function closeAll(a, b) {
+		var notClose = '100';
+		const d = dropdown[a];
+		$( d ).animate({ height: "0" }, 200 );
+		setTimeout(ontimeout, 250);
+		function ontimeout() {
+			while (d.firstChild) {
+				d.removeChild(d.firstChild);
 			}
-		} console.log(b); 
-		if(buttons[b].lastChild.textContent === 'Done') {
-			buttons[b].removeChild(buttons[b].lastChild);
+		} if (buttons[a].lastChild.textContent === 'Done') {
+			buttons[a].removeChild(buttons[a].lastChild);
 		}
-		firstClick = true;
+		firstClick[b] = true;
 	}
-
-
-			// clearContent();
-
-			// const content1 = document.createElement('p');
-			// const content2 = document.createElement('div');
-			// const content3 = document.createElement('input');
-			// const content4 = document.createElement('button');
-			// const content5 = document.createElement('div');
-
-			// content1.textContent = "Enter The Question Below";
-			// content3.placeholder = "Please Enter A Question";
-			// content2.appendChild(content3);
-			// content4.textContent = "Done";
-			// content5.appendChild(content4);
-			// content5.classList.add("sendToPreview");
-
-
-			// modalTextContent.appendChild(content1);
-			// modalTextContent.appendChild(content2);
-			// modalTextContent.appendChild(content5);
-
-			// togglePlaceholderOnClick();
-
-			// /* Send Question To Preview */
-			// content4.addEventListener('click', function () {
-
-			// 	if (content3.value) {
-			// 		const questionObject = {
-			// 			question: content3.value.includes('?') ? content3.value : content3.value + '?',
-			// 		}
-			// 		createPreview(1, questionObject, questionId);
-			// 		questionId++;
-			// 		modal.style.display = "none";
-			// 	}
-			// });
-
 	// 		/* specific modal creation if the "add a one option question" button was pressed */
 	// 	else if (buttonId[1] === '2') {
 
